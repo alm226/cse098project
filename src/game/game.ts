595ea091c46e0eea6b97ddf86e6bc9df60149725
Alex Margulies <alm226@lehigh.edu>
2024-01-18 13:35:00 -0500

    removed spaces in theme name

--- src/game/game.ts
+++ src/game/game.ts
@@ -15,7 +15,7 @@ class Config implements JetLagGameConfig {
   storageKey = "com.github.alm226.cse098project";
   hitBoxes = false;
   resourcePrefix = "./assets/";
-  musicNames = ["Ghosted The Game theme.wav"];
+  musicNames = ["GhostedTheGameTheme.wav"];
   soundNames = [];
   imageNames = ["tile_1.png", "tile_2.png", "key.png", "pause.png", "restart.png", "endLevel.png",
     "pcPortrait.png", "npcPortrait.png", "collect.png", "back_arrow.png", "right_arrow.png",
